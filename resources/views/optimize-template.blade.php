4edd9b3a6a0186f49a8d367bf9923b7985155869
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-02-19 20:43:35 +0700

    Optimize Template (End)

---
+++ resources/views/optimize-template.blade.php
@@ -0,0 +1,4 @@
+{{-- Jangan lupa untuk melakukan "php artisan view:cache" agar nanti viewnya akan ke kompile --}}
+{{-- Gunakan "php artisan view:clear" untuk menghapus hasil compile jika ada perubahan --}}
+{{-- Hal tersebut dilakukan agar nanti jika aplikasi sudah masuk pada mode deploy di server laravelnya tidak perlu
+melakukan compiling view lagi yang akibatnnya website akan jadi lambat --}}
