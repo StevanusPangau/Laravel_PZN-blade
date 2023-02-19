aa89126436fbf4d0084ed2bee5752293549e21ea
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-02-19 20:24:00 +0700

    Extending Blade

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\SayHello;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // directive seperit kita membuat function blade sendiri yang tinggal dipanggil nantinya
+        Blade::directive("hello", function ($expression) {
+            return "<?php echo 'Hello ' . $expression ?>";
+        });
     }
 }
---
+++ tests/Feature/ExtendingBladeTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ExtendingBladeTest extends TestCase
+{
+    public function testExtendingBlade()
+    {
+        $this->view("extending", [
+            "name" => "Evan"
+        ])->assertSeeText("Hello Evan");
+    }
+}
